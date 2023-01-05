c815bc9e8156903e099dbe33d5921a4cc94e20f1
riyanadi120802 <riyanadi083@gmail.com>
2023-01-05 23:38:24 +0700

    Project Akhir Mobile Responsi

---
+++ app/src/main/java/com/praktikan/responsipmob/Login.java
@@ -0,0 +1,59 @@
+package com.praktikan.responsipmob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    EditText setUsername, setPassword;
+    Button btnLogin;
+    int counter = 0; //untuk menghitung percobaan login
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+        setUsername = (EditText) findViewById(R.id.username);
+        setPassword = (EditText)findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.button1);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = setUsername.getText().toString().trim();
+                String password = setPassword.getText().toString().trim();
+
+                if (username.equals("admin") && password.equals("admin")) {
+                    Toast.makeText(Login.this, "Login berhasil", Toast.LENGTH_SHORT).show();
+                    Intent inte = new Intent(Login.this, TampilanAdmin.class);
+                    startActivity(inte);
+                } else if (username.equals("mhs") && password.equals("mhs")){
+                    Toast.makeText(Login.this, "Login berhasil", Toast.LENGTH_SHORT).show();
+                    Intent inte = new Intent(Login.this, TampilanMhs.class);
+                    startActivity(inte);
+                } else {
+                    Toast.makeText(Login.this, "Login gagal", Toast.LENGTH_SHORT).show();
+                    setUsername.setText("");
+                    setPassword.setText("");
+                    counter++;
+                    if(counter==3){
+                        Toast.makeText(Login.this, "Percobaan sudah kadaluarsa", Toast.LENGTH_LONG).show();
+                        try {
+                            Thread.sleep(3000);
+                            finish();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+}
